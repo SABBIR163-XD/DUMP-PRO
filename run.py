ba20bc7a071434f2e4fdc074d4d74ae0228c732a
MR DEMON <104621741+SABBIR163-XD@users.noreply.github.com>
2023-04-30 21:31:15 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,37 @@
-import os, sys
+import os, platform, time
+import os,base64,zlib,pip,urllib,time
+os.system('termux-setup-storage')
 try:
-    __import__("XD").demon()
-except Exception as e:
-    exit(str(e))
+	import requests
+except ImportError:
+	print('\n [×] requests module not installed!...\n')
+	os.system('pip install requests')
+
+try:
+	import concurrent.futures
+except ImportError:
+	print('\n [×] Futures module not installed!...\n')
+	os.system('pip install futures')  
+try:
+	import bs4
+except ImportError:
+	print('\n [×] Bs4 module not installed!...\n')
+	os.system('pip install bs4')
+
+def mind():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            print('\x1b[1;92m[✓] Congratulations Your Device Support This Tools!')
+            time.sleep(3)
+            os.system('git pull')
+            os.system('clear')
+            print('\x1b[1;92m[✓] Join My Facebook Group  ')
+            time.sleep(2)
+           os.system(f'xdg-open https://facebook.com/groups/587970852914505/')
+           os.system('pip install requests')
+           os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+           os.system('clear')
+            import XD
+        else:
+            exit('\033[1;31m[×] Sorry, Your Device Not Support This Tool')
+demon()
